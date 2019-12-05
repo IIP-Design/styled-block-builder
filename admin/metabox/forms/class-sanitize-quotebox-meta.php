61569299ab44aa8f2b0292834009e48cf42f217f
mrewers <mrewers@users.noreply.github.com>
2019-12-05 16:44:33 -0500

    Remove unneccessary stripslashes

--- admin/metabox/forms/class-sanitize-quotebox-meta.php
+++ admin/metabox/forms/class-sanitize-quotebox-meta.php
@@ -10,23 +10,23 @@ class Sanitize_Quotebox_Meta {
     $sanitized = array();
 
     if( !empty( $unsanitary['title'] ) ) {
-      $sanitized['title'] = stripslashes( sanitize_text_field( $unsanitary['title'] ) );
+      $sanitized['title'] = sanitize_text_field( $unsanitary['title'] );
     }
 
     if( !empty( $unsanitary['subtitle'] ) ) {
-      $sanitized['subtitle'] = stripslashes( sanitize_textarea_field( $unsanitary['subtitle'] ) );
+      $sanitized['subtitle'] = sanitize_textarea_field( $unsanitary['subtitle'] );
     }
 
     if( !empty( $unsanitary['desc'] ) ) {
-      $sanitized['desc'] = stripslashes( sanitize_textarea_field( $unsanitary['desc'] ) );
+      $sanitized['desc'] = sanitize_textarea_field( $unsanitary['desc'] );
     }
 
     if( !empty( $unsanitary['quote'] ) ) {
-      $sanitized['quote'] = stripslashes( sanitize_textarea_field( $unsanitary['quote'] ) );
+      $sanitized['quote'] = sanitize_textarea_field( $unsanitary['quote'] );
     }
 
     if( !empty( $unsanitary['speaker'] ) ) {
-      $sanitized['speaker'] = stripslashes( sanitize_textarea_field( $unsanitary['speaker'] ) );
+      $sanitized['speaker'] = sanitize_textarea_field( $unsanitary['speaker'] );
     }
 
     return $sanitized;
