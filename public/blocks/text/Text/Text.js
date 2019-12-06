6848b25e6851c9f826d439c1cb5900b5b10c2e79
Steven Hascher <shascher@gmail.com>
2019-12-06 15:57:49 -0500

    update Text block with module css

--- public/blocks/text/Text/Text.js
+++ public/blocks/text/Text/Text.js
@@ -2,7 +2,7 @@ import React from 'react';
 import propTypes from 'prop-types';
 import Button from '../../_shared/Button/Button';
 
-import './Text.scss';
+import './Text.module.scss';
 import mockData from './mockdata';
 
 const Text = ( { id } ) => {
@@ -10,11 +10,11 @@ const Text = ( { id } ) => {
   const { description, buttonText, link, style, arrow, subtitle, title } = mockData;
 
   return (
-    <div className="gpalab-temp-text-container">
-      <h2 className="gpalab-temp-text-title">{ title }</h2>
-      <h3 className="gpalab-temp-text-subtitle">{ subtitle }</h3>
-      <div className="gpalab-temp-text-content">
-        <div className="gpalab-temp-text-description">{ description }</div>
+    <div styleName="container">
+      <h2 styleName="title">{ title }</h2>
+      <h3 styleName="subtitle">{ subtitle }</h3>
+      <div styleName="content">
+        <div styleName="description">{ description }</div>
       </div>
       { buttonText && link && <Button link={ link } text={ buttonText } style={ style } arrow={ arrow } /> }
     </div>
