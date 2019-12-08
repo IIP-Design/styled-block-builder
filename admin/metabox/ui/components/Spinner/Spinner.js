c01ce6a722a7492d20d9419cb82c3978f8705a87
mrewers <mrewers@users.noreply.github.com>
2019-12-07 19:08:52 -0500

    Add Spinner component

---
+++ admin/metabox/ui/components/Spinner/Spinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './Spinner.module.scss';
+
+const Spinner = () => (
+  <div styleName="container">
+    <div styleName="background" />
+    <div styleName="spinner" />
+  </div>
+);
+
+export default Spinner;
