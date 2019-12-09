426f7ed68d93b5176413b17cbbcc6b48f17a0103
mrewers <mrewers@users.noreply.github.com>
2019-12-09 13:57:01 -0500

    Pull shared content out of layouts and hook up conditional rendering of sections

--- public/blocks/resources/Resources/Layouts/BaseLayout.js
+++ public/blocks/resources/Resources/Layouts/BaseLayout.js
@@ -1,22 +1,12 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import propTypes from 'prop-types';
 
-import CDPFeed from '../../../_shared/components/CDPFeed/CDPFeed';
+import './Layouts.module.scss';
 
 const BaseLayout = ( { data } ) => (
-  <div className="resource-section hidden" id={ data.id }>
-    <div className="resource-section-content" id={ data.id }>
-      <div className="resource-content no-columns">
-        <p className="resource-content-title">{ data.title }</p>
-        <div className="resource-content-text">{ data.text }</div>
-      </div>
-      { data.cdp && (
-        <Fragment>
-          <hr className="resource-hr" />
-          <CDPFeed id={ data.id } items={ data.cdp } />
-        </Fragment>
-      ) }
-    </div>
+  <div styleName="content no-columns">
+    <p styleName="content-title">{ data.title }</p>
+    <div styleName="content-text">{ data.text }</div>
   </div>
 );
 
--- public/blocks/resources/Resources/Layouts/VideoLayout.js
+++ public/blocks/resources/Resources/Layouts/VideoLayout.js
@@ -1,38 +1,20 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import propTypes from 'prop-types';
 
-import CDPFeed from '../../../_shared/components/CDPFeed/CDPFeed';
+import VideoEmbed from '../../../_shared/components/VideoEmbed/VideoEmbed';
+
+import './Layouts.module.scss';
 
 const VideoLayout = ( { data } ) => (
-  <div className="resource-section" id={ data.id }>
-    <div className="resource-section-content" id={ data.id }>
-      <div className="resource-content-columns">
-        <div className="video">
-          <div className="video-responsive">
-            <iframe
-              title={ data.title }
-              src={ data.video }
-              frameBorder="0"
-              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            />
-          </div>
-        </div>
-        <div className="star-line">
-          <span className="dashicons dashicons-star-filled" />
-          <span className="dashicons dashicons-star-filled" />
-          <span className="dashicons dashicons-star-filled" />
-        </div>
-        <p className="resource-content-title">{ data.title }</p>
-        <div className="resource-content-text">{ data.text }</div>
-      </div>
-      { data.cdp && (
-        <Fragment>
-          <hr className="resource-hr" />
-          <CDPFeed id={ data.id } items={ data.cdp } />
-        </Fragment>
-      ) }
+  <div styleName="content-columns">
+    <VideoEmbed title={ data.title } url={ data.video } />
+    <div styleName="star-line">
+      <span className="dashicons dashicons-star-filled" />
+      <span className="dashicons dashicons-star-filled" />
+      <span className="dashicons dashicons-star-filled" />
     </div>
+    <p styleName="content-title">{ data.title }</p>
+    <div styleName="content-text">{ data.text }</div>
   </div>
 );
 
