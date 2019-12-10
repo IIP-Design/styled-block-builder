4dc6c9774b31e28867f81f3d1d93f3d74a8bb784
Steven Hascher <shascher@gmail.com>
2019-12-09 20:08:35 -0500

    apply animation using useEffect

--- public/blocks/stats/Stats/Stats.js
+++ public/blocks/stats/Stats/Stats.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import propTypes from 'prop-types';
 import { TimelineMax, Power2 } from 'gsap';
 import ScrollMagic from 'scrollmagic';
@@ -20,10 +20,76 @@ const Stats = ( { id } ) => {
       title
     } = meta;
 
+    useEffect( () => {
+      const runStats = () => {
+        const tl = new TimelineMax(); // Set up new timeline for tweens
+
+        const statOne = document.getElementById( 'stat-1' );
+        const statTwo = document.getElementById( 'stat-2' );
+        const statThree = document.getElementById( 'stat-3' );
+
+        console.log( statOne );
+
+        // Initialize counters with 0 values
+        const counter = {
+          one: { val: 0 },
+          two: { val: 0 },
+          three: { val: 0 }
+        };
+
+        // Change the value displayed by the stats block
+        const countUp = ( el, count ) => {
+          el.innerHTML = Math.ceil( count.val );
+        };
+
+        if ( statOne && statTwo && statThree ) {
+          tl.to( counter.one, 5, {
+            val: statOne.getAttribute( 'data-stat' ),
+            onUpdate: () => countUp( statOne, counter.one ),
+            ease: Power2.easeOut
+          } )
+            .to(
+              counter.two,
+              5,
+              {
+                val: statTwo.getAttribute( 'data-stat' ),
+                onUpdate: () => countUp( statTwo, counter.two ),
+                ease: Power2.easeOut
+              },
+              0 // Start without delay
+            )
+            .to(
+              counter.three,
+              5,
+              {
+                val: statThree.getAttribute( 'data-stat' ),
+                onUpdate: () => countUp( statThree, counter.three ),
+                ease: Power2.easeOut
+              },
+              0 // Start without Delay
+            );
+        }
+      };
+      // Set the animation trigger using Scroll Magic
+      const start = document.querySelector( '.stats-container' );
+
+      const controller = new ScrollMagic.Controller();
+
+      if ( start ) {
+        new ScrollMagic.Scene( {
+          triggerElement: start,
+          triggerHook: 'onCenter',
+          duration: '100%'
+        } )
+          .setTween( runStats )
+          .addTo( controller );
+      }
+    } );
+
     return (
       <div styleName="box-bg" style={ { backgroundImage: `url(${background})` } }>
         <div styleName="opacity-overlay" id="stats-section" />
-        <div styleName="container">
+        <div className="stats-container" styleName="container">
           { title && <h2 styleName="title">{ title }</h2> }
           <div styleName="array">
             { statOneNumber && statOneText && (
@@ -67,70 +133,6 @@ const Stats = ( { id } ) => {
 
   return null;
 };
-
-const statOne = document.getElementById( 'stat-1' );
-const statTwo = document.getElementById( 'stat-2' );
-const statThree = document.getElementById( 'stat-3' );
-
-// Initialize counters with 0 values
-const counter = {
-  one: { val: 0 },
-  two: { val: 0 },
-  three: { val: 0 }
-};
-
-// Change the value displayed by the stats block
-const countUp = ( el, count ) => {
-  el.innerHTML = Math.ceil( count.val );
-};
-
-// Run the stats animation
-const runStats = () => {
-  const tl = new TimelineMax(); // Set up new timeline for tweens
-
-  if ( statOne && statTwo && statThree ) {
-    tl.to( counter.one, 5, {
-      val: statOne.getAttribute( 'data-stat' ),
-      onUpdate: () => countUp( statOne, counter.one ),
-      ease: Power2.easeOut
-    } )
-      .to(
-        counter.two,
-        5,
-        {
-          val: statTwo.getAttribute( 'data-stat' ),
-          onUpdate: () => countUp( statTwo, counter.two ),
-          ease: Power2.easeOut
-        },
-        0 // Start without delay
-      )
-      .to(
-        counter.three,
-        5,
-        {
-          val: statThree.getAttribute( 'data-stat' ),
-          onUpdate: () => countUp( statThree, counter.three ),
-          ease: Power2.easeOut
-        },
-        0 // Start without Delay
-      );
-  }
-};
-
-// Set the animation trigger using Scroll Magic
-const start = document.querySelector( '.gpalab-box-bg-3fy_h' );
-
-const controller = new ScrollMagic.Controller();
-
-if ( start ) {
-  new ScrollMagic.Scene( {
-    triggerElement: start,
-    triggerHook: 'onCenter',
-    duration: '100%'
-  } )
-    .setTween( runStats )
-    .addTo( controller );
-}
 
 Stats.propTypes = {
   id: propTypes.string
