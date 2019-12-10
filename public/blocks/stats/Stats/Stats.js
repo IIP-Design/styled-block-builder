8a87925685fecf0d447df81caada42f6b4d257c8
Steven Hascher <shascher@gmail.com>
2019-12-10 11:23:24 -0500

    comment out scrollmagic functionality

--- public/blocks/stats/Stats/Stats.js
+++ public/blocks/stats/Stats/Stats.js
@@ -21,57 +21,55 @@ const Stats = ( { id } ) => {
     } = meta;
 
     useEffect( () => {
-      const runStats = () => {
-        const tl = new TimelineMax(); // Set up new timeline for tweens
+      // const runStats = () => {
+      const tl = new TimelineMax(); // Set up new timeline for tweens
 
-        const statOne = document.getElementById( 'stat-1' );
-        const statTwo = document.getElementById( 'stat-2' );
-        const statThree = document.getElementById( 'stat-3' );
+      const statOne = document.getElementById( 'stat-1' );
+      const statTwo = document.getElementById( 'stat-2' );
+      const statThree = document.getElementById( 'stat-3' );
 
-        console.log( statOne );
-
-        // Initialize counters with 0 values
-        const counter = {
-          one: { val: 0 },
-          two: { val: 0 },
-          three: { val: 0 }
-        };
-
-        // Change the value displayed by the stats block
-        const countUp = ( el, count ) => {
-          el.innerHTML = Math.ceil( count.val );
-        };
-
-        if ( statOne && statTwo && statThree ) {
-          tl.to( counter.one, 5, {
-            val: statOne.getAttribute( 'data-stat' ),
-            onUpdate: () => countUp( statOne, counter.one ),
-            ease: Power2.easeOut
-          } )
-            .to(
-              counter.two,
-              5,
-              {
-                val: statTwo.getAttribute( 'data-stat' ),
-                onUpdate: () => countUp( statTwo, counter.two ),
-                ease: Power2.easeOut
-              },
-              0 // Start without delay
-            )
-            .to(
-              counter.three,
-              5,
-              {
-                val: statThree.getAttribute( 'data-stat' ),
-                onUpdate: () => countUp( statThree, counter.three ),
-                ease: Power2.easeOut
-              },
-              0 // Start without Delay
-            );
-        }
+      // Initialize counters with 0 values
+      const counter = {
+        one: { val: 0 },
+        two: { val: 0 },
+        three: { val: 0 }
       };
+
+      // Change the value displayed by the stats block
+      const countUp = ( el, count ) => {
+        el.innerHTML = Math.ceil( count.val );
+      };
+
+      if ( statOne && statTwo && statThree ) {
+        tl.to( counter.one, 5, {
+          val: statOne.getAttribute( 'data-stat' ),
+          onUpdate: () => countUp( statOne, counter.one ),
+          ease: Power2.easeOut
+        } )
+          .to(
+            counter.two,
+            5,
+            {
+              val: statTwo.getAttribute( 'data-stat' ),
+              onUpdate: () => countUp( statTwo, counter.two ),
+              ease: Power2.easeOut
+            },
+            0 // Start without delay
+          )
+          .to(
+            counter.three,
+            5,
+            {
+              val: statThree.getAttribute( 'data-stat' ),
+              onUpdate: () => countUp( statThree, counter.three ),
+              ease: Power2.easeOut
+            },
+            0 // Start without Delay
+          );
+      }
+      // };
       // Set the animation trigger using Scroll Magic
-      const start = document.querySelector( '.stats-container' );
+      /*  const start = document.querySelector( '.stats-container' );
 
       const controller = new ScrollMagic.Controller();
 
@@ -83,7 +81,7 @@ const Stats = ( { id } ) => {
         } )
           .setTween( runStats )
           .addTo( controller );
-      }
+      } */
     } );
 
     return (
