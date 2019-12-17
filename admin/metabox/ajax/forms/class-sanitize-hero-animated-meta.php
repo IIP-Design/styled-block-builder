acf41d01cc375533610b390e227a8c3e5b1ba279
Steven Hascher <30932943+shascher@users.noreply.github.com>
2019-12-17 12:02:27 -0500

    Update hero animated sanitizer

--- admin/metabox/ajax/forms/class-sanitize-hero-animated-meta.php
+++ admin/metabox/ajax/forms/class-sanitize-hero-animated-meta.php
@@ -13,9 +13,7 @@ class Sanitize_HeroAnimated_Meta {
       $sanitized_lines = array();
 
       foreach ( $unsanitary['lines'] as $line ) {
-        $sanitized_line = array();
-
-        $sanitized_line['line'] = sanitize_text_field( $line['line'] );
+        $sanitized_line = sanitize_text_field( $line['line'] );
         
 
         array_push( $sanitized_lines, $sanitized_line );
