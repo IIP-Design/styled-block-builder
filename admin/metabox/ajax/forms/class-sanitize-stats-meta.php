8a5ccf5632957a3fdbef3e210f760a9ea1d69fa5
mrewers <mrewers@users.noreply.github.com>
2019-12-30 17:08:18 -0500

    Alphabetize

--- admin/metabox/ajax/forms/class-sanitize-stats-meta.php
+++ admin/metabox/ajax/forms/class-sanitize-stats-meta.php
@@ -9,30 +9,23 @@ class Sanitize_Stats_Meta {
     $unsanitary = json_decode( stripslashes( $data ), true );
     $sanitized = array();
 
-    if( !empty( $unsanitary['backgroundType'] ) ) {
-      $sanitized['backgroundType'] = sanitize_text_field( $unsanitary['backgroundType'] );
-    }
-
-    if( !empty( $unsanitary['backgroundImage'] ) ) {
+    if ( !empty( $unsanitary['backgroundImage'] ) ) {
       $sanitized['backgroundImage'] = sanitize_text_field( $unsanitary['backgroundImage'] );
     }
 
-    if( !empty( $unsanitary['blockBackground'] ) ) {
+    if ( !empty( $unsanitary['backgroundType'] ) ) {
+      $sanitized['backgroundType'] = sanitize_text_field( $unsanitary['backgroundType'] );
+    }
+    
+    if ( !empty( $unsanitary['blockBackground'] ) ) {
       $sanitized['blockBackground'] = sanitize_text_field( $unsanitary['blockBackground'] );
     }
 
-    if( !empty( $unsanitary['fullWidth'] ) ) {
+    if ( !empty( $unsanitary['fullWidth'] ) ) {
       $sanitized['fullWidth'] = rest_sanitize_boolean( $unsanitary['fullWidth'] );
     }
 
-    if( !empty( $unsanitary['textColor'] ) ) {
-      $sanitized['textColor'] = sanitize_text_field( $unsanitary['textColor'] );
-    }
-    
-    if( !empty( $unsanitary['title'] ) ) {
-      $sanitized['title'] = sanitize_text_field( $unsanitary['title'] );
-    }
-    if( !empty( $unsanitary['stats'] ) ) {
+    if ( !empty( $unsanitary['stats'] ) ) {
       $sanitized_stats = array();
 
       foreach ( $unsanitary['stats'] as $stat ) {
@@ -48,6 +41,14 @@ class Sanitize_Stats_Meta {
       unset($stat);
 
       $sanitized['stats'] = $sanitized_stats;
+    }
+    
+    if ( !empty( $unsanitary['textColor'] ) ) {
+      $sanitized['textColor'] = sanitize_text_field( $unsanitary['textColor'] );
+    }
+    
+    if ( !empty( $unsanitary['title'] ) ) {
+      $sanitized['title'] = sanitize_text_field( $unsanitary['title'] );
     }
 
     return $sanitized;
