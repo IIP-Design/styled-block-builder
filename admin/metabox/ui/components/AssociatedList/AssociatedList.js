03dffbd5d1c50a3785696c2081d2dd6f37b31c97
mrewers <mrewers@users.noreply.github.com>
2020-01-04 16:56:54 -0500

    Place template list in a MetaboxContext object and create reducer to manipulate it

--- admin/metabox/ui/components/AssociatedList/AssociatedList.js
+++ admin/metabox/ui/components/AssociatedList/AssociatedList.js
@@ -1,40 +1,44 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useContext, useState } from 'react';
 import propTypes from 'prop-types';
 
+import { MetaboxContext } from 'metabox/components/Metabox/Metabox';
 import { updatePost } from 'metabox/utils/update-post';
 
 import './AssociatedList.module.scss';
 
-const AssociatedList = ( { list, edit, updateMetabox } ) => {
-  const [updating, setUpdating] = useState( [] );
+const AssociatedList = ({ list, edit, updateMetabox }) => {
+  const [updating, setUpdating] = useState([]);
+
+  const { dispatch, state } = useContext(MetaboxContext);
+  const { templates } = state;
 
   const deleteItem = async id => {
     const newUpdating = [...updating];
-    newUpdating.push( id );
-    setUpdating( newUpdating );
+    newUpdating.push(id);
+    setUpdating(newUpdating);
 
-    await updatePost( { id }, 'delete' );
+    await updatePost({ id }, 'delete');
 
-    const resetUpdating = newUpdating.filter( item => item !== id );
-    setUpdating( resetUpdating );
-    updateMetabox( id, 'delete' );
+    const resetUpdating = newUpdating.filter(item => item !== id);
+    setUpdating(resetUpdating);
+    updateMetabox(id, 'delete');
   };
 
   return (
     <Fragment>
       <h4 styleName="header">Existing Templates for This Post:</h4>
       <div styleName="list">
-        { list.map( item => (
+        {templates.map(item => (
           <div
-            data-id={ item.id }
-            key={ item.id }
-            styleName={ updating.includes( item.id ) ? 'list-item disabled' : 'list-item' }
+            data-id={item.id}
+            key={item.id}
+            styleName={updating.includes(item.id) ? 'list-item disabled' : 'list-item'}
           >
-            { item.title || item.id }
+            {item.title || item.id}
             <button
               aria-label="edit template"
-              disabled={ item.id === updating }
-              onClick={ () => edit( item.id, item.type ) }
+              disabled={item.id === updating}
+              onClick={() => edit(item.id, item.type)}
               styleName="button"
               type="button"
             >
@@ -42,15 +46,15 @@ const AssociatedList = ( { list, edit, updateMetabox } ) => {
             </button>
             <button
               aria-label="delete template"
-              disabled={ item.id === updating }
-              onClick={ () => deleteItem( item.id ) }
-              styleName={ updating.includes( item.id ) ? 'button disabled' : 'button' }
+              disabled={item.id === updating}
+              onClick={() => dispatch({ type: 'delete-item', payload: item.id })}
+              styleName={updating.includes(item.id) ? 'button disabled' : 'button'}
               type="button"
             >
               <span className="dashicons dashicons-trash" />
             </button>
           </div>
-        ) ) }
+        ))}
       </div>
     </Fragment>
   );
--- admin/metabox/ui/components/Metabox/Metabox.js
+++ admin/metabox/ui/components/Metabox/Metabox.js
@@ -1,41 +1,68 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 
 import AssociatedList from 'metabox/components/AssociatedList/AssociatedList';
 import Modal from 'metabox/components/Modal/Modal';
 
 import './Metabox.module.scss';
 
-const MetaBox = () => {
-  const [formId, setFormId] = useState( 0 );
-  const [formType, setFormType] = useState( '' );
-  const [showModal, setShowModal] = useState( false );
-  const [associated, setAssociated] = useState( [] );
+const MetaboxContext = React.createContext();
+const { Provider } = MetaboxContext;
 
-  useEffect( () => {
+const initialState = {
+  templates: []
+};
+
+function metaboxReducer(state, action) {
+  switch (action.type) {
+    case 'init':
+      return { templates: action.payload };
+    case 'delete-item':
+      console.log(`delete ${action.payload}`);
+      break;
+    default:
+      throw new Error();
+  }
+}
+
+const MetaBox = () => {
+  const [formId, setFormId] = useState(0);
+  const [formType, setFormType] = useState('');
+  const [showModal, setShowModal] = useState(false);
+  const [associated, setAssociated] = useState([]);
+
+  const [state, dispatch] = useReducer(metaboxReducer, initialState);
+
+  const store = {
+    dispatch,
+    state,
+    getState: () => state
+  };
+
+  useEffect(() => {
     const list = window?.gpalabTemplateAdmin?.associated
       ? window.gpalabTemplateAdmin.associated
       : [];
 
-    setAssociated( list );
-  }, [] );
+    dispatch({ type: 'init', payload: list });
+  }, []);
 
   const toggleModal = () => {
-    setShowModal( false );
-    setFormId( 0 );
-    setFormType( '' );
+    setShowModal(false);
+    setFormId(0);
+    setFormType('');
   };
 
-  const editExisting = ( id, type ) => {
-    setFormId( id );
-    setFormType( type );
-    setShowModal( true );
+  const editExisting = (id, type) => {
+    setFormId(id);
+    setFormType(type);
+    setShowModal(true);
   };
 
-  const updateAssociated = ( res, action ) => {
+  const updateAssociated = (res, action) => {
     const clone = [...associated];
 
-    if ( action === 'save' ) {
-      const filtered = clone.filter( items => items.id !== res.id );
+    if (action === 'save') {
+      const filtered = clone.filter(items => items.id !== res.id);
 
       const newItem = {
         id: res.id,
@@ -44,59 +71,62 @@ const MetaBox = () => {
         type: `gpalab-${res.post_type}`
       };
 
-      filtered.push( newItem );
+      filtered.push(newItem);
 
-      setAssociated( filtered );
+      setAssociated(filtered);
     }
 
-    if ( action === 'delete' ) {
-      const filtered = clone.filter( items => items.id !== res );
+    if (action === 'delete') {
+      const filtered = clone.filter(items => items.id !== res);
 
-      setAssociated( filtered );
+      setAssociated(filtered);
     }
   };
 
   return (
     <div styleName="dropdown-container">
-      <label htmlFor="gpalab-templates-dropdown">
-        <strong>Add Template:</strong>
-        <select
-          styleName="dropdown"
-          id="gpalab-templates-dropdown"
-          onChange={ e => setFormType( e.target.value ) }
-          value={ formType }
+      <Provider value={store}>
+        <label htmlFor="gpalab-templates-dropdown">
+          <strong>Add Template:</strong>
+          <select
+            styleName="dropdown"
+            id="gpalab-templates-dropdown"
+            onChange={e => setFormType(e.target.value)}
+            value={formType}
+          >
+            <option value="">- Select Template Type -</option>
+            <option value="article-feed">Article Feed</option>
+            <option value="hero">Hero Block</option>
+            <option value="parallax">Parallax Block</option>
+            <option value="quote-box">Quote Box</option>
+            <option value="resources">Resources Block</option>
+            <option value="slides">Slides Block</option>
+            <option value="stats">Stats Block</option>
+            <option value="text">Text Block</option>
+            <option value="timeline">Timeline Block</option>
+          </select>
+        </label>
+        <button
+          className="button-secondary"
+          disabled={formType === ''}
+          onClick={() => setShowModal(true)}
+          type="button"
         >
-          <option value="">- Select Template Type -</option>
-          <option value="article-feed">Article Feed</option>
-          <option value="hero">Hero Block</option>
-          <option value="parallax">Parallax Block</option>
-          <option value="quote-box">Quote Box</option>
-          <option value="resources">Resources Block</option>
-          <option value="slides">Slides Block</option>
-          <option value="stats">Stats Block</option>
-          <option value="text">Text Block</option>
-          <option value="timeline">Timeline Block</option>
-        </select>
-      </label>
-      <button
-        className="button-secondary"
-        disabled={ formType === '' }
-        onClick={ () => setShowModal( true ) }
-        type="button"
-      >
-        Configure Template
-      </button>
-      { associated.length > 0 && (
-        <AssociatedList list={ associated } edit={ editExisting } updateMetabox={ updateAssociated } />
-      ) }
-      <Modal
-        id={ formId }
-        form={ formType }
-        show={ showModal }
-        toggle={ toggleModal }
-        updateMetabox={ updateAssociated }
-      />
+          Configure Template
+        </button>
+        {state.templates.length > 0 && (
+          <AssociatedList list={associated} edit={editExisting} updateMetabox={updateAssociated} />
+        )}
+        <Modal
+          id={formId}
+          form={formType}
+          show={showModal}
+          toggle={toggleModal}
+          updateMetabox={updateAssociated}
+        />
+      </Provider>
     </div>
   );
 };
-export default MetaBox;
+
+export { MetaboxContext, MetaBox as default };
