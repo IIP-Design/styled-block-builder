791ec66261cb1a8ca995c49ef588d9f9c42a2dc5
mrewers <mrewers@users.noreply.github.com>
2020-01-10 18:07:55 -0500

    Handle removal of nested files

--- admin/metabox/ui/components/FileUploader/FileUploader.js
+++ admin/metabox/ui/components/FileUploader/FileUploader.js
@@ -2,47 +2,64 @@ import React, { useContext } from 'react';
 import propTypes from 'prop-types';
 
 import { AdminContext } from 'metabox/context/adminContext';
+import { checkForFile } from 'metabox/utils/helpers';
 
 import './FileUploader.module.scss';
 
-const FileUploader = ({ current, label, name, parentGroup, parentId }) => {
-  const { dispatch } = useContext(AdminContext);
+const FileUploader = ({ label, name, parentGroup, parentId }) => {
+  const { dispatch, state } = useContext(AdminContext);
+  const formValues = state?.formData?.formValues ? state.formData.formValues : {};
 
   const handleFile = e => {
-    const inputName = e.target.name;
     const file = e.target.files[0];
 
     if (parentGroup) {
       dispatch({
         type: 'file-add-nested',
-        payload: { file, name: inputName, parentGroup, parentId }
+        payload: { file, name, parentGroup, parentId }
       });
     } else {
-      dispatch({ type: 'file-add', payload: { file, name: inputName } });
+      dispatch({ type: 'file-add', payload: { file, name } });
     }
   };
 
-  return (
-    <label htmlFor="quote-box-image" styleName="label">
-      {label}
-      {current && (
-        <div styleName="existing-file">
-          <p>
-            <strong>Current image: </strong>
-            {current.filename}
-          </p>
-          <button styleName="button" type="button">
-            Remove This File
-          </button>
-        </div>
-      )}
-      {!current && <input name={name} type="file" onChange={e => handleFile(e)} />}
-    </label>
-  );
+  const handleFileRemoval = () => {
+    if (parentGroup) {
+      dispatch({
+        type: 'file-remove-nested',
+        payload: { name, parentGroup, parentId }
+      });
+    } else {
+      dispatch({ type: 'file-remove', payload: { name } });
+    }
+  };
+
+  if (formValues) {
+    const current = checkForFile(name, formValues, parentGroup, parentId);
+
+    return (
+      <label htmlFor="quote-box-image" styleName="label">
+        {label}
+        {current && (
+          <div styleName="existing-file">
+            <p>
+              <strong>Current image: </strong>
+              {current.filename || 'Pending upload...'}
+            </p>
+            <button styleName="button" type="button" onClick={() => handleFileRemoval()}>
+              Remove This File
+            </button>
+          </div>
+        )}
+        {!current && <input name={name} type="file" onChange={e => handleFile(e)} />}
+      </label>
+    );
+  }
+
+  return null;
 };
 
 FileUploader.propTypes = {
-  current: propTypes.object,
   label: propTypes.string,
   name: propTypes.string,
   parentGroup: propTypes.string,
@@ -50,7 +67,6 @@ FileUploader.propTypes = {
 };
 
 FileUploader.defaultProps = {
-  current: null,
   label: 'Upload a file:',
   parentGroup: null,
   parentId: null
