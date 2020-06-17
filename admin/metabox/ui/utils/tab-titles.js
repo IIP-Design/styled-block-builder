1fd4be565013a999dee2fcc9cd392e6581e91ab6
mrewers <mrewers@users.noreply.github.com>
2020-06-17 17:33:05 -0400

    Add tests for tab title helper functions

--- admin/metabox/ui/utils/tab-titles.js
+++ admin/metabox/ui/utils/tab-titles.js
@@ -1,3 +1,10 @@
+/**
+ * Adjusts the length of a tab's title depending on the number of tabs
+ * @param number Index of the given tab
+ * @param {number} length Number of tabs
+ * @param {string} title Tab title
+ * @returns {string|number} Abbreviated title with ellipses appended to the end (if needed)
+ */
 export const responsiveTitle = ( index, length, title ) => {
   if ( !title || length > 7 ) {
     return index + 1;
@@ -10,9 +17,14 @@ export const responsiveTitle = ( index, length, title ) => {
   // -1 to account for 0 based indexing, rounded to whole number
   const end = Math.round( base / length - 1 );
 
-  return `${title.substring( 0, end )}...`;
+  return title.length > end ? `${title.substring( 0, end )}...` : title;
 };
 
+/**
+ * Finds the title field from an array of field data objects
+ * @param {Object[]} list A list of field data
+ * @returns {string} The name of the title field
+ */
 export const getTabTitleField = list => {
   let title;
 
