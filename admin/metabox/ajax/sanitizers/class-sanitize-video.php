12a370ff8ec3e6e7872165c775ce0db2f86bba1b
mrewers <mrewers@users.noreply.github.com>
2020-08-07 15:03:05 -0400

    Add dedicated sanitizer for video inputs

---
+++ admin/metabox/ajax/sanitizers/class-sanitize-video.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Registers the Sanitize_Files class.
+ *
+ * @package Style_Blocks\Sanitizer
+ * @since 0.0.1
+ */
+
+namespace Style_Blocks;
+
+/**
+ * Iterate over uploaded files and compare.
+ *
+ * Checks for and sanitizes the fields expected by the video form.
+ *
+ * @package Style_Blocks\Sanitizer
+ * @since 2.0.0
+ */
+class Sanitize_Video {
+
+  /**
+   * Checks for and sanitizes the expected fields.
+   *
+   * @param array $video    Unsanitized video values sent over in the AJAX request.
+   * @return array          Array of sanitized values
+   */
+  public function sanitize_video( $video ) {
+    $sanitized_video = array();
+
+    $sanitized_video['id']          = sanitize_text_field( $video['id'] );
+    $sanitized_video['description'] = wp_kses_post( $video['description'] );
+    $sanitized_video['title']       = sanitize_text_field( $video['title'] );
+    $sanitized_video['url']         = sanitize_text_field( $video['url'] );
+
+    return $sanitized_video;
+  }
+
+}
