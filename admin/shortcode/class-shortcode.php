677c861e3b9cebc544665a14aa1ac18846aa0fb3
mrewers <mrewers@users.noreply.github.com>
2020-08-11 17:57:06 -0400

    Add id to every block on the front end

--- admin/shortcode/class-shortcode.php
+++ admin/shortcode/class-shortcode.php
@@ -62,7 +62,7 @@ class Shortcode {
     );
 
     // Generate and return the shortcode.
-    $html = '<div data-id="' . $id . '" data-type="gpalab-' . $type . '"></div>';
+    $html = '<div data-id="' . $id . '" data-type="gpalab-' . $type . '" id="gpalab-' . $id . '"></div>';
 
     return $html;
   }
