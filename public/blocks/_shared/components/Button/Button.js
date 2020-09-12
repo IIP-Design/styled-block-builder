afedf1167b745a4f437ee641ef494d9d3ebbddc9
mrewers <mrewers@users.noreply.github.com>
2020-09-12 16:56:53 -0400

    feat: conditionally render the the prefix text if addPrefix prop is equal to true

--- public/blocks/_shared/components/Button/Button.js
+++ public/blocks/_shared/components/Button/Button.js
@@ -16,17 +16,24 @@ const Button = ( { config } ) => {
 
   return (
     <a href={ link } styleName={ `button ${color} ${border}` }>
-      { text }
-      { arrow && arrow !== 'none' && (
-        <span styleName={ `arrow arrow-${arrow}` }>
-          <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 985 512" role="presentation">
-            <defs />
-            <path styleName="svg" d="M688.939 48.406l36.644-36.644 244.047 244.047-36.644 36.643-244.047-244.047z" />
-            <path styleName="svg" d="M688.858 463.533l244.047-244.047 36.644 36.643-244.047 244.047-36.644-36.644z" />
-            <path styleName="svg" d="M25.911 230.089h880.971v51.822h-880.971v-51.822z" />
-          </svg>
-        </span>
+      { addPrefix && prefix && (
+        <div>
+          { prefix }
+        </div>
       ) }
+      <div>
+        { text }
+        { arrow && arrow !== 'none' && (
+          <span styleName={ `arrow arrow-${arrow}` }>
+            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 985 512" role="presentation">
+              <defs />
+              <path styleName="svg" d="M688.939 48.406l36.644-36.644 244.047 244.047-36.644 36.643-244.047-244.047z" />
+              <path styleName="svg" d="M688.858 463.533l244.047-244.047 36.644 36.643-244.047 244.047-36.644-36.644z" />
+              <path styleName="svg" d="M25.911 230.089h880.971v51.822h-880.971v-51.822z" />
+            </svg>
+          </span>
+        ) }
+      </div>
     </a>
   );
 };
