ee5486149da4d805e3d3015ee4e103901ee99a64
mrewers <mrewers@users.noreply.github.com>
2020-09-25 14:53:16 -0400

    refactor: make chevron stroke configurable

--- public/blocks/_shared/components/Chevron/Chevron.js
+++ public/blocks/_shared/components/Chevron/Chevron.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import propTypes from 'prop-types';
 
-const Chevron = ( { reverse } ) => (
+const Chevron = ( { reverse, stroke } ) => (
   <span className="icon-svg" style={ reverse ? { transform: 'rotateX(180deg)' } : {} }>
     <svg width="18" height="10" viewBox="0 0 18 10" xmlns="http://www.w3.org/2000/svg">
-      <g stroke="#B63031" strokeWidth="1.5" fill="none" fillRule="evenodd" strokeLinecap="square">
+      <g stroke={ stroke } strokeWidth="1.5" fill="none" fillRule="evenodd" strokeLinecap="square">
         <path d="M8.879 8.879L1.12 1.12M9 9l8-8" />
       </g>
     </svg>
@@ -13,6 +13,11 @@ const Chevron = ( { reverse } ) => (
 
 Chevron.propTypes = {
   reverse: propTypes.bool,
+  stroke: propTypes.string,
+};
+
+Chevron.defaultProps = {
+  stroke: '#B63031',
 };
 
 export default Chevron;
