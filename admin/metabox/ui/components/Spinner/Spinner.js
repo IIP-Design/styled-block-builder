23fde67de530776eab261991bd596c434edfe841
mrewers <mrewers@users.noreply.github.com>
2020-09-29 14:36:03 -0400

    refactor: make spinner wheel it's own component so it can be used elsewhere

--- admin/metabox/ui/components/Spinner/Spinner.js
+++ admin/metabox/ui/components/Spinner/Spinner.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import Wheel from 'metabox/components/Spinner/Wheel';
+
 import './Spinner.module.scss';
 
 const Spinner = () => (
   <div styleName="container">
     <div styleName="spinner-background">
       <strong>Saving...</strong>
-      <div styleName="spinner" />
+      <Wheel />
     </div>
   </div>
 );
---
+++ admin/metabox/ui/components/Spinner/Wheel.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './Spinner.module.scss';
+
+const Wheel = () => (
+  <div styleName="spinner" />
+);
+
+export default Wheel;
