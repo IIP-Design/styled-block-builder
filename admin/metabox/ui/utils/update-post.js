a609b7fda032506492a388500144f0addd72be0a
mrewers <mrewers@users.noreply.github.com>
2020-11-27 15:01:46 -0500

    refactor: break out Ajax response handler into another function

--- admin/metabox/ui/utils/update-post.js
+++ admin/metabox/ui/utils/update-post.js
@@ -1,5 +1,39 @@
 import { getFormData } from './helpers';
 
+/**
+ * Utility function to handle the Ajax response from the server.
+ *
+ * @param {Object} response The server response as a JSON object.
+ * @param {string} action The type of action to be taken (save/delete/primary).
+ * @param {onComplete} onComplete A function to be run once the Ajax request completes successfully.
+ * @param {onError} onError A function to be run if an error is encountered during the Ajax request.
+ */
+const handleResponse = ( response, action, onComplete, onError ) => {
+  // PHP may return a 200 is AJAX call is received but requested action fails
+  // for this reason we check the success message on the response
+  if ( response.success === false ) {
+    if ( onError ) {
+      onError( response.data );
+    } else {
+      console.error( response.data );
+    }
+  }
+
+  if ( response.success !== false && onComplete ) {
+    const res = response?.data?.data ? response.data.data : null;
+
+    onComplete( res, action );
+  }
+};
+
+/**
+ * Sends an Ajax request to the server to update/delete a given block.
+ *
+ * @param {Object} data The block data to be saved.
+ * @param {string} action The type of action to be taken (save/delete).
+ * @param {onComplete} onComplete A function to be run once the Ajax request completes successfully.
+ * @param {onError} onError A function to be run if an error is encountered during the Ajax request.
+ */
 export const updatePost = async ( data, action, onComplete, onError ) => {
   // Get values provided to the client by the server
   const fromPHP = window?.gpalabBlockAdmin || {};
@@ -65,17 +99,7 @@ export const updatePost = async ( data, action, onComplete, onError ) => {
     } );
     const result = await response.json();
 
-    // PHP may return a 200 is AJAX call is received but requested action fails
-    // for this reason we check the success message on the response
-    if ( result.success === false && onError ) {
-      onError( result.data );
-    }
-
-    if ( result.success !== false && onComplete ) {
-      const res = result?.data?.data ? result.data.data : null;
-
-      onComplete( res, action );
-    }
+    handleResponse( result, action, onComplete, onError );
   } catch ( error ) {
     if ( onError ) {
       onError( error );
